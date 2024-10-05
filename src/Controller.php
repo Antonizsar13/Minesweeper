3fe9aee8622ad3aae066fcdca033388a263f3cd9
antonizsar13 <antonizsar13@gmail.com>
2024-10-05 12:35:10 +0300

    first commit

---
+++ src/Controller.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Antonzisar13\Minesweeper\Controller;
+
+use Antonzisar13\Minesweeper\View;
+
+function startGame() {
+    View\startScreen();
+}
---
+++ src/View.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Antonzisar13\Minesweeper\View;
+
+function startScreen() {
+    \cli\line("Welcome to Minesweeper!");
+}
