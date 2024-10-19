00b2ca19421e264a9038bbc17e73b5cc20b8ab87
antonizsar13 <antonizsar13@gmail.com>
2024-10-19 18:20:54 +0300

    v2

--- src/Controller.php
+++ src/Controller.php
@@ -1,9 +1,47 @@
 <?php
 
-namespace Antonzisar13\Minesweeper\Controller;
+namespace Anton\Minesweeper;
 
-use Antonzisar13\Minesweeper\View;
+class Controller
+{
+    private $game;
+    private $view;
 
-function startGame() {
-    View\startScreen();
+    public function __construct(Game $game, View $view)
+    {
+        $this->game = $game;
+        $this->view = $view;
+    }
+
+    public function startGame($saveToDatabase = false)
+    {
+        $this->view->startScreen();
+        if (!$saveToDatabase) {
+            \cli\line("Примечание: Игра пока не сохраняется в базе данных.");
+        }
+        while (!$this->game->isGameOver()) {
+            $this->view->showMap($this->game->getGameMap());
+
+            $input = $this->view->promptCoordinates();
+            if (strpos($input, ',') === false) {
+                $this->view->invalidInput();
+                continue;
+            }
+
+            list($x, $y) = explode(',', $input);
+            $x = (int)trim($x);
+            $y = (int)trim($y);
+
+            if ($x < 0 || $x >= $this->game->getSize() || $y < 0 || $y >= $this->game->getSize()) {
+                $this->view->invalidCoordinates();
+                continue;
+            }
+
+            $result = $this->game->play($x, $y);
+            if ($result) {
+                $this->view->gameOver($result);
+                break;
+            }
+        }
+    }
 }
--- src/View.php
+++ src/View.php
@@ -1,7 +1,48 @@
 <?php
 
-namespace Antonzisar13\Minesweeper\View;
+namespace Anton\Minesweeper;
 
-function startScreen() {
-    \cli\line("Welcome to Minesweeper!");
+class View
+{
+    public function startScreen()
+    {
+        \cli\line("Welcome to Minesweeper!");
+    }
+
+    public function showMap($gameMap)
+    {
+        $size = count($gameMap);
+        \cli\line("  " . implode(" ", range(0, $size - 1)));
+        foreach ($gameMap as $lineN => $line) {
+            \cli\out($lineN . ' ');
+            foreach ($line as $point) {
+                \cli\out($point . ' ');
+            }
+            \cli\line();
+        }
+    }
+
+    public function gameOver($result)
+    {
+        if ($result == 'lost') {
+            \cli\line('Game Over! Вы подорвались. :(');
+        } else {
+            \cli\line('Поздравляем! Победа!');
+        }
+    }
+
+    public function promptCoordinates()
+    {
+        return trim(\cli\prompt("Введите координаты (x, y):"));
+    }
+
+    public function invalidInput()
+    {
+        \cli\line("Неправильный формат. Введите в формате 'x, y'.");
+    }
+
+    public function invalidCoordinates()
+    {
+        \cli\line("Неправильные координаты!");
+    }
 }
